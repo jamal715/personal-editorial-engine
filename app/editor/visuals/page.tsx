eea41c557d7885f5e721e892e223a35c81211e68
Jamal Nasir <117292470+jamal715@users.noreply.github.com>
2026-09-06 15:59:23 +0500

    Add visual intelligence workspace route

---
+++ app/editor/visuals/page.tsx
@@ -0,0 +1,5 @@
+import VisualIntelligenceLab from "../../../components/VisualIntelligenceLab";
+
+export const metadata={title:"Visual Intelligence | Jamal Nasir",robots:{index:false,follow:false}};
+
+export default function VisualsPage(){return <VisualIntelligenceLab/>}
