e7465021cf4b4d3350f94d4c4667e9a9abb75e48
Jamal Nasir <117292470+jamal715@users.noreply.github.com>
2026-09-07 01:48:14 +0500

    Switch Visual Intelligence workspace to V3

--- app/editor/visuals/page.tsx
+++ app/editor/visuals/page.tsx
@@ -1,5 +1,5 @@
-import VisualIntelligenceEngineV2 from "../../../components/VisualIntelligenceEngineV2";
+import VisualIntelligenceEngineV3 from "../../../components/VisualIntelligenceEngineV3";
 
 export const metadata={title:"Visual Intelligence | Jamal Nasir",robots:{index:false,follow:false}};
 
-export default function VisualsPage(){return <VisualIntelligenceEngineV2/>}
+export default function VisualsPage(){return <VisualIntelligenceEngineV3/>}
