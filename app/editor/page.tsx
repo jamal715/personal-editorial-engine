941bc1ee8fa640b52a02742898a2ea7ebfd29aba
Jamal Nasir <117292470+jamal715@users.noreply.github.com>
2026-09-06 14:05:59 +0500

    Switch editor to rewrite intelligence v5

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,5 +1,5 @@
-import EditorialIntelligenceStudio from "../../components/EditorialIntelligenceStudio";
+import EditorialIntelligenceStudioV5 from "../../components/EditorialIntelligenceStudioV5";
 
 export const metadata={title:"Editorial Intelligence | Jamal Nasir",robots:{index:false,follow:false}};
 
-export default function EditorPage(){return <EditorialIntelligenceStudio/>}
+export default function EditorPage(){return <EditorialIntelligenceStudioV5/>}
