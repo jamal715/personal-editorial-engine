47518f41d1131accb89a8ced19e5624133b993a4
Jamal Nasir <117292470+jamal715@users.noreply.github.com>
2026-09-06 16:13:11 +0500

    Activate visual intelligence recommendation engine

--- app/editor/visuals/page.tsx
+++ app/editor/visuals/page.tsx
@@ -1,5 +1,5 @@
-import VisualIntelligenceLab from "../../../components/VisualIntelligenceLab";
+import VisualIntelligenceEngineV2 from "../../../components/VisualIntelligenceEngineV2";
 
 export const metadata={title:"Visual Intelligence | Jamal Nasir",robots:{index:false,follow:false}};
 
-export default function VisualsPage(){return <VisualIntelligenceLab/>}
+export default function VisualsPage(){return <VisualIntelligenceEngineV2/>}
