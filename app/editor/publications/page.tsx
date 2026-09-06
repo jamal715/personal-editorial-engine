c5e24e00f62bf69bf35f05b81f7e8162557307b9
Jamal Nasir <117292470+jamal715@users.noreply.github.com>
2026-09-06 20:04:19 +0500

    Add protected publication manager page

---
+++ app/editor/publications/page.tsx
@@ -0,0 +1,1 @@
+import PublicationManager from "../../../components/PublicationManager";export const metadata={title:"Publications | Jamal Nasir",robots:{index:false,follow:false}};export default function Page(){return <PublicationManager/>}
