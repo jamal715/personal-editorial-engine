4cdc95736a86f84300d52c7137f2c0e42ec165e7
Jamal Nasir <117292470+jamal715@users.noreply.github.com>
2026-09-06 17:41:11 +0500

    Add simple compose editor route

---
+++ app/editor/compose/page.tsx
@@ -0,0 +1,5 @@
+import SimpleArticleComposer from "../../../components/SimpleArticleComposer";
+
+export const metadata={title:"Compose | Editorial Intelligence",robots:{index:false,follow:false}};
+
+export default function Page(){return <SimpleArticleComposer/>}
