c8c8a3eb58047e5a56c31c84113ec82bd11b108a
Jamal Nasir <117292470+jamal715@users.noreply.github.com>
2026-09-07 01:48:04 +0500

    Add visual research preview route

---
+++ app/editor/visuals/preview/page.tsx
@@ -0,0 +1,3 @@
+import VisualIntelligencePreview from "../../../../components/VisualIntelligencePreview";
+export const metadata={title:"Visual research preview | Jamal Nasir",robots:{index:false,follow:false}};
+export default function Page(){return <VisualIntelligencePreview/>}
